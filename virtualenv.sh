6278d47c197332104eb6626e77bce06a1ffe58c7
Dion Bosschieter <dbosschieter@transip.nl>
2019-08-27 12:49:06 +0200

    Fix virtual env bootstrap

--- virtualenv.sh
+++ virtualenv.sh
@@ -1,6 +1,8 @@
+#!/bin/bash -xe
 # bootstrap virtualenv
 export VIRTUAL_ENV=.virtualenv/krakenex
 mkdir -p $VIRTUAL_ENV
 virtualenv $VIRTUAL_ENV
 source $VIRTUAL_ENV/bin/activate
+$VIRTUAL_ENV/bin/pip install -r requirements.txt
 
